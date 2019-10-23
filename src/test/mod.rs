f443e8f231a62c91d01f46811634bc618d2b56de
Youness <youness.kafia@devinci.fr>
2019-10-23 16:35:27 +0200

    Display is working

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -7,27 +7,32 @@ use super::{
     Bus
 };
 
-pub fn test_cpu(cpu : OLC6502, bus : Bus ){
-    let mut x : Vec<u8> = Vec::new();
-    for _ in 0..64{
-        x.push(random());
-    }
-    println!("{}",display_ram(x, 3, 16));
-    
+pub fn test_cpu(cpu : &OLC6502, bus : &Bus ){
 
+    println!("{}\n{}",display_ram(bus.ram.to_vec(), 16, 3),display_registers(cpu));
+    
 }   
 
 /// Display ram on a length * depth
 fn display_ram(ram : Vec<u8>, length : usize, depth : usize) -> String {
     let mut result = String::new();
-    for i in 64..128{
-        result+= format!("{1:00$x}\n",4,i).as_str();
+
+    for i in 0..depth*length{
+        if i % length == 0{
+            result+= format!("\n{1:00$X}  --  ",4,i).as_str();
+        }
+        result+= format!(" {1:00$X}",2,ram[i]).as_str();
     }
-    // for i in 0..depth{
-        
-    //     result+= format!(" {:x} ",ram[i]).as_str();
-        
-    // }
     result
     
+}
+
+fn display_registers(cpu : &OLC6502) -> String{
+    let mut result = String::new();
+    result += format!("{:012} : {:0}\n","Accumulator",cpu.a).as_str();
+    result += format!("{:012} : {:0}\n","X-Register",cpu.x).as_str();
+    result += format!("{:012} : {:0}\n","Y-Register",cpu.y).as_str();
+    result += format!("{:012} : {:0}\n","StackPointer",cpu.stkp).as_str();
+    result += format!("{:012} : {:0}\n","Status",cpu.status).as_str();
+    result
 }
