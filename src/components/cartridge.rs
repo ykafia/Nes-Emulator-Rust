eb812c2a5e38f382a06bd9624512159c03d2e003
KAFIA Youness <youness.kafia-ext@coface.com>
2020-01-02 14:32:48 +0100

    Added a variant for cartridge headers

--- src/components/cartridge.rs
+++ src/components/cartridge.rs
@@ -2,14 +2,15 @@ use std::ops::{Index,IndexMut};
 use std::fs::File;
 use std::io::prelude::*;
 use super::*;
-
+#[warn(non_camel_case_types)] 
 pub struct Cartridge {
     pub data : Vec<u8>,
     pub program_rom : Vec<u8>,
     pub character_rom : Vec<u8>,
     pub mapper_id : u8,
     pub prgm_bank_n : u8,
-    pub chr_bank_n : u8
+    pub chr_bank_n : u8,
+    pub header : ROM
 }
 
 impl Cartridge {
@@ -21,11 +22,19 @@ impl Cartridge {
             mapper_id : 0,
             prgm_bank_n : 0,
             chr_bank_n : 0,
+            header : ROM::None
         }
     }
     pub fn load(&mut self, pathfile:&str) {
         let mut file = File::open(pathfile).unwrap();
         file.read_to_end(&mut self.data).unwrap();
+
+        if self.data[7] & 0x0C == 0 && self.data[12..15].to_vec().iter().sum::<u8>() == 0 {
+            self.header = ROM::new_ines(self.data.to_vec()); 
+        } else if self.data[7] & 0x0C == 0x08 {
+            self.header = ROM::new_nes2(self.data.to_vec());
+        }
+        
     }
     pub fn len(&self) -> usize{
         self.data.len()
@@ -52,37 +61,127 @@ impl IndexMut<usize> for Cartridge{
 }
 
 
-struct CartridgeHeader {
-    name : [u8;4],
-    prg_rom_size : u8,
-    chr_rom_size : u8,
-    /// Mapper, mirroring, battery, trainer
-    flag6 : u8,
-    /// Mapper, VS/Playchoice, NES 2.0
-    flag7 : u8,
-    /// Program size
-    flag8 : u8,
-    /// TV system (rarely used extension)
-    flag9 : u8,
-    /// TV system, PRG-RAM presence (unofficial, rarely used extension)
-    flag10 : u8,
-    /// Unused padding (should be filled with zero, but some rippers put their name across bytes 7-15)
-    padding : [u8;8]
+
+
+pub enum ROM{
+    ARCHAIC ,
     
+    iNES {
+        name : [u8;4],
+        prg_rom_size : u8,
+        chr_rom_size : u8,
+        /// Mapper, mirroring, battery, trainer
+        flag6 : u8,
+        /// Mapper, VS/Playchoice, NES 2.0
+        flag7 : u8,
+        /// Program size
+        flag8 : u8,
+        /// TV system (rarely used extension)
+        flag9 : u8,
+        /// TV system, PRG-RAM presence (unofficial, rarely used extension)
+        flag10 : u8,
+    },
+    NES2 {
+        /// [0-3]  Name. 
+        name : [u8;4],
+        /// [4] Program rom size 
+        prg_rom_size : u8,
+        /// [5] CHR rom size 
+        chr_rom_size : u8,
+        /// [6] Mapper, mirroring, battery, trainer
+        flag6 : u8,
+        /// [7] Mapper, VS/Playchoice, NES 2.0
+        flag7 : u8,
+        /// [8] Mapper value for NES2.0 files, byte 8
+        mapper : u8,
+        /// [9] PRG + CHR rom size contained in one byte, PRG -> low, CHR -> high
+        rm_sizes_msb : u8,
+        /// [10] PRG-RAM/EEPROM size.
+        /// low -> PRG-RAM (volatile) shift count.
+        /// high -> PRG-NVRAM/EEPROM (non-volatile) shift count.
+        /// If the shift count is zero, there is no PRG-(NV)RAM.
+        /// If the shift count is non-zero, the actual size is
+        /// "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7. 
+        eeprom_size : u8,
+        /// [11] low -> CHR-RAM size (volatile) shift count
+        /// high -> CHR-NVRAM size (non-volatile) shift count
+        /// If the shift count is zero, there is no CHR-(NV)RAM.
+        /// If the shift count is non-zero, the actual size is
+        /// "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7.
+        chr_ram_size : u8,
+        /// [12] CPU/PPU Timing 
+        /// only 4 different values (only 2 lowest bits used)
+        /// 0: RP2C02 ("NTSC NES")
+        /// 1: RP2C07 ("Licensed PAL NES")
+        /// 2: Multiple-region
+        /// 3: UMC 6527P ("Dendy")
+        cpu_ppu_timing : u8,
+        /// [13] Vs. System Type (when data[7] & 3 = 3)
+        /// low : PPU Type
+        /// high : hardware type
+        vs_system_type : u8,
+        /// [13] Extended Console Type (when data[7] & 3 =3)
+        extended_console_type : u8,
+        /// [14] Miscellaneous ROMs : Number of miscellaneous ROMs present (max = 3)
+        misc_roms : u8,
+        /// [15] Default Expansion Device
+        default_expansion_dvc : u8
+
+    },
+    None
 }
 
-impl CartridgeHeader {
-    fn new() -> CartridgeHeader{
-        CartridgeHeader {
-            name : [0x4E,0x45,0x53,0x1A],
-            prg_rom_size : 0,
-            chr_rom_size : 0,
-            flag6 : 0,
-            flag7 : 0,
-            flag8 : 0,
-            flag9 : 0,
-            flag10 : 0,
-            padding : [0;8]
+impl ROM {
+    fn new_ines(data : Vec<u8>) -> ROM {
+        ROM::iNES {
+            name : [ 
+                data[0],
+                data[1],
+                data[2],
+                data[3]
+            ],
+            prg_rom_size : data[4],
+            chr_rom_size : data[5],
+            /// Mapper, mirroring, battery, trainer
+            flag6 : data[6],
+            /// Mapper, VS/Playchoice, NES 2.0
+            flag7 : data[7],
+            /// Program size
+            flag8 : data[8],
+            /// TV system (rarely used extension)
+            flag9 : data[9],
+            /// TV system, PRG-RAM presence (unofficial, rarely used extension)
+            flag10 : data[10],
+        }
+    }
+    fn new_nes2(data : Vec<u8>) -> ROM {
+        ROM::NES2 {
+            name : [ 
+                data[0],
+                data[1],
+                data[2],
+                data[3]
+            ],
+            prg_rom_size : data[4],
+            chr_rom_size : data[5],
+            flag6 : data[6],
+            flag7 : data[7],
+            mapper : data[8],
+            rm_sizes_msb : data[9],
+            eeprom_size : data[10],
+            chr_ram_size : data[11],
+            cpu_ppu_timing : data[12],
+            vs_system_type : match data[7] & 3 == 1 {
+                true => data[13],
+                false => 0
+            },
+            extended_console_type : match data[7] & 3 == 3 {
+                true => data[13],
+                false => 0
+            },
+            misc_roms : data[14],
+            default_expansion_dvc : data[15]
+            
         }
     }
 }
